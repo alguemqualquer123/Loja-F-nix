372045b02bde8e7315d0c4c420f466d08a00ec3c
Dev` Juniors <136682675+alguemqualquer123@users.noreply.github.com>
2024-05-10 04:45:37 -0300

    alteração do link do auth discord

--- src/config.js
+++ src/config.js
@@ -9,4 +9,4 @@ export const api = axios.create({
   }
 });
 
-export const authDiscord  = "https://discord.com/oauth2/authorize?client_id=1091900395418308610&response_type=code&redirect_uri=https%3A%2F%2Fauthdinamic.fenixdevelopment.com.br%2Fapi%2Fdiscord&scope=identify+connections+guilds+gdm.join+guilds.members.read+guilds.join+email"
+export const authDiscord  = "https://discord.com/oauth2/authorize?client_id=1091900395418308610&response_type=code&redirect_uri=https%3A%2F%2Fauthdinamic.fenixdevelopment.com.br%2Fapi%2Fdiscord&scope=email+connections+identify"
