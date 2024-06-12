f37091431ea9ae1f66efc4abb8d89dca3d56c644
Dev` Juniors <136682675+alguemqualquer123@users.noreply.github.com>
2024-06-12 04:12:57 -0300

    add ignore error in vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,26 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    {
+      name: 'ignore-errors',
+      enforce: 'pre',
+      apply: 'build',
+      buildStart() {
+        this.warn = () => {};  // Ignora todos os warnings
+        this.error = (msg) => {  // Ignora todos os erros
+          console.warn('Ignorando erro:', msg);
+        };
+      }
+    }
+  ],
   resolve: {
     alias: {
       "@app": path.resolve(__dirname, "./src"),
       "@": path.resolve(__dirname, "./src"),
     },
   },
+ 
   optimizeDeps: {
     exclude: ["@fortawesome/fontawesome-svg-core"],
   },
